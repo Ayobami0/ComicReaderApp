e2c1cd034b4041bddf34eae2d5c87e8c704a6654
Ayobami <oludemiayobami@gmail.com>
2023-01-19 00:09:52 +0100

    started work on comic chapter page

--- lib/pages/comic_chapter_page.dart
+++ lib/pages/comic_chapter_page.dart
@@ -1,27 +1,50 @@
+import 'package:comic_reader/comics.dart';
+import 'package:comic_reader/widgets/image.dart';
 import 'package:flutter/material.dart';
 
-class ChapterPage extends StatefulWidget {
-  const ChapterPage({super.key});
+class ChapterPage extends StatelessWidget {
+  final Comic comic;
+  final String imageUrl;
 
-  @override
-  State<ChapterPage> createState() => _ChapterPageState();
-}
+  const ChapterPage({super.key, required this.comic, required this.imageUrl});
 
-class _ChapterPageState extends State<ChapterPage> {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      builder: ((BuildContext context, AsyncSnapshot snapshot) {
+    return Scaffold(
+        appBar: AppBar(
+
+        ),
+        body: FutureBuilder(
+        builder: ((BuildContext context, AsyncSnapshot snapshot) {
           List<Widget> children = [];
-          if (snapshot.hasData){
+          if (snapshot.hasData) {
+            List data = snapshot.data;
             children = <Widget>[
-              
+              Text(comic.title),
+              ImageWidget(imageUrl: imageUrl),
+              Row(
+                children: [
+                  Text(comic.views),
+                ],
+              ),
+              ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (BuildContext context, int index){
+                    return Card(
+                      child: ListTile(
+                        onTap: () {},
+                        title: Text(data[index]['name']),
+                      ),
+                    );
+                  }
+              )
             ];
           }
           return Column(
             children: children,
           );
-        }
+        }),
+        future: comic.getComicChapters(),
       ),
     );
   }
